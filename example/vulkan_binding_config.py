775d9bd11a2a6438f4009e8fd748a492d8bd614a
Oleg Plakhotniuk <olegus8@gmail.com>
2020-11-12 17:22:14 -0500

    working on modules

--- example/vulkan_binding_config.py
+++ example/vulkan_binding_config.py
@@ -21,13 +21,5 @@ class Config(ConfigBase):
 
     def configure_struct_field(self, field):
         #FIXME: make it work for all fields
-        if (field.name.startswith('pfn')
-            or (field.is_array and ('char' in field.type))
-        ):
+        if field.name.startswith('pfn'):
             field.ignore()
-        for kw in [
-            #TODO: add size_t and char to ast_typedecl.h
-            'unsigned long',
-        ]:
-            if kw in field.type:
-                field.ignore()
