2a0c7f822600825d7244686d338f6a4049a1da9d
Oleg Plakhotniuk <olegus8@gmail.com>
2020-11-13 17:50:52 -0500

    working on modules

--- example/vulkan_binding_config.py
+++ example/vulkan_binding_config.py
@@ -38,6 +38,7 @@ class Config(ConfigBase):
         or  func.name.endswith('INTEL')
         or  func.name.endswith('AMD')
         or  func.name.endswith('GOOGLE')
+        or  func.name.endswith('NV')
         ):
             func.ignore()
         
