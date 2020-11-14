2831968b429da417e62c270b0c43116b1186f017
Oleg Plakhotniuk <olegus8@gmail.com>
2020-11-14 07:03:58 -0500

    working on modules

--- example/vulkan_module.cpp
+++ example/vulkan_module.cpp
@@ -2,7 +2,7 @@
 
 #include "vulkan/vulkan.h"
 
-struct VulkanHandleAnnotation : DummyTypeTypeAnnotation {
+struct VulkanHandleAnnotation : DummyTypeAnnotation {
     virtual bool isRefType() const override { return false; }
     virtual bool isLocal() const override { return true; }
     virtual bool canCopy() const override { return true; }
