85bf4ccab95d546d0e2dd111ae2a93ef59e30469
Oleg Plakhotniuk <olegus8@gmail.com>
2020-11-14 14:57:29 -0500

    working on modules

---
+++ example/app_module.cpp
@@ -0,0 +1,28 @@
+#include "daScript/daScript.h"
+
+using namespace das;
+
+struct SmartObject : public das::ptr_ref_count {
+};
+
+MAKE_TYPE_FACTORY(SmartObject, SmartObject)
+
+struct SmartObjectAnnotation : ManagedStructureAnnotation <SmartObject> {
+    SmartObjectAnnotation(ModuleLibrary & ml) : ManagedStructureAnnotation ("SmartObject", ml) {
+    }
+    virtual bool isLocal() const override { return false; }
+    virtual bool canMove() const override { return false; }
+    virtual bool canCopy() const override { return false; }
+};
+
+class Module_app : public Module {
+public:
+    Module_app() : Module("app") {
+        ModuleLibrary lib;
+        lib.addModule(this);
+        lib.addBuiltInModule();
+        addAnnotation(make_smart<TestObjectSmartAnnotation>(lib));
+    }
+};
+
+REGISTER_MODULE(Module_app);
--- example/main.cpp
+++ example/main.cpp
@@ -94,6 +94,7 @@ int main( int argc, char * argv[] ) {
     // register modules
     NEED_ALL_DEFAULT_MODULES;
     NEED_MODULE(Module_vulkan);
+    NEED_MODULE(Module_app);
 
     uint64_t timeStamp = ref_time_ticks();
     bool ok = true;
