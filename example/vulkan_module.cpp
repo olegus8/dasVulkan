d86234519b54bc40226ba602233130cc1129b6fb
Oleg Plakhotniuk <olegus8@gmail.com>
2020-11-14 07:58:22 -0500

    working on modules

--- example/vulkan_module.cpp
+++ example/vulkan_module.cpp
@@ -5,19 +5,12 @@
 using namespace das;
 
 template <typename VK_TYPE>
-struct VulkanHandleAnnotation : DummyTypeAnnotation {
-    VulkanHandleAnnotation(const string & name, const string & cppName, size_t sz, size_t al)
-        : DummyTypeAnnotation(name, cppName, sz, al) {
+struct VulkanHandleAnnotation : ManagedValueAnnotation<VK_TYPE> {
+    VulkanHandleAnnotation(const string & name, const string & cppName)
+        : ManagedValueAnnotation(name, cppName) {
     }
-    virtual bool isRefType() const override { return false; }
-    virtual bool isLocal() const override { return true; }
-    virtual bool canCopy() const override { return true; }
-    virtual bool canMove() const override { return true; }
-    virtual bool canClone() const override { return true; }
     virtual bool canNew() const override { return true; }
     virtual bool canDelete() const override { return true; }
-    virtual bool isPod() const override { return true; }
-    virtual bool isRawPod() const override { return true; }
     virtual SimNode * simulateGetNew ( Context & context, const LineInfo & at ) const override {
         return context.code->makeNode<SimNode_NewHandle<VK_TYPE,false>>(at);
     }
