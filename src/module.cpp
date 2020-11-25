333a02d8a1ef35d85599c155c054568d76a9f2f6
Oleg Plakhotniuk <olegus8@gmail.com>
2020-11-25 09:10:56 -0500

    working on modules

---
+++ src/module.cpp
@@ -0,0 +1,7 @@
+#include "daScript/daScript.h"
+
+#include "headers_to_bind.h"
+
+using namespace das;
+
+#include "module_generated.inc"
--- src/vulkan_module.cpp
+++
@@ -1,7 +0,0 @@
-#include "daScript/daScript.h"
-
-#include "vulkan_headers_to_bind.h"
-
-using namespace das;
-
-#include "vulkan_module_generated.inc"
