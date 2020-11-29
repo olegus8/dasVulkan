bd71313d2669085ec69e31157fdf000b7a7f141d
Oleg Plakhotniuk <olegus8@gmail.com>
2020-11-29 07:33:40 -0500

    working on modules

--- src/boost_generator.py
+++ src/boost_generator.py
@@ -97,6 +97,7 @@ class VkHandle(object):
                f'struct {self.__boost_batch_type}',
                f'    {self.__boost_batch_attr} : '
                         f'array<{self.__das_handle_type}>',
+                '',
                f'def split(batch : {self.__boost_batch_type}) '
                     f': array<self.__boost_type>',
                f'    return <- [{{for h in batch.{self.__boost_batch_attr} ;',
