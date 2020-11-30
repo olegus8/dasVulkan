498995cf8739cd1be2956bb6479b831d0fa80c23
Oleg Plakhotniuk <olegus8@gmail.com>
2020-11-30 15:46:50 -0500

    working on modules

--- src/boost_generator.py
+++ src/boost_generator.py
@@ -73,6 +73,7 @@ class BoostGenerator(LoggingObject):
             BoostVkHandleType,
             BoostVkStructPtrType,
             BoostStringType,
+            BoostStringPtrType,
             BoostUInt32Type,
             BoostUnknownType,
         ]:
@@ -503,6 +504,18 @@ class BoostStringType(BoostType):
     @property
     def name(self):
         return 'string'
+
+
+class BoostStringPtrType(BoostType):
+
+    @classmethod
+    def maybe_create(cls, c_type_name, **kwargs):
+        if c_type_name == 'const char *const *':
+            return cls(c_type_name=c_type_name, **kwargs)
+
+    @property
+    def name(self):
+        return 'string ?'
 
 
 class BoostUInt32Type(BoostType):
