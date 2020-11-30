02ce69d352743abf3c7b541eaaa11ee866774e45
Oleg Plakhotniuk <olegus8@gmail.com>
2020-11-30 12:24:54 -0500

    working on modules

--- src/boost_generator.py
+++ src/boost_generator.py
@@ -103,7 +103,7 @@ class VkHandle(object):
 
     @property
     def __vk_dtor_params(self):
-        return self.__func_params_ex(self.__vk_ctor)
+        return self.__func_params_ex(self.__vk_dtor)
 
     def __func_params_ex(self, vk_func):
         return map(ParamEx, vk_func.params)
