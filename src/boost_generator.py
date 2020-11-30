a30a9675cc2028a701ac9594e8f1625bca083f62
Oleg Plakhotniuk <olegus8@gmail.com>
2020-11-30 11:29:13 -0500

    working on modules

--- src/boost_generator.py
+++ src/boost_generator.py
@@ -3,6 +3,9 @@ from das_shared.assertions import assert_starts_with
 import re
 
 
+#TODO: add pAllocator support
+
+
 class VulkanBoostError(Exception):
     pass
 
@@ -247,6 +250,74 @@ class VkHandle(object):
            f'    var handles <- {self.__boost_enumerator}({params_text})',
             '    defer() <| ${ delete handles; }',
            f'    return <- handles |> split()',
+        ]
+        return lines
+
+    def __generate_ctor(self):
+        lines = []
+        lines += [
+            '',
+           f'def {self.__boost_ctor}(']
+        for param in self.__vk_ctor_params:
+            if param.vk.name == 'pAllocator':
+                continue
+            elif param.vk.name == self.__p_create_info:
+                lines += [f'    {param.boost.name} : {param.boost.type_deref}']
+            else:
+                raise Exception(f'TODO: add support for extra param '
+                    f'{param.vk.name}')
+                lines += [f'    {param.boost.name} : {param.boost.type};']
+        lines += [
+           f'    var result : VkResult? = [[VkResult?]]',
+           f') : {self.__boost_type}',
+            '',
+           f'    var {self.__boost_attr} : {self.__boost_type}',
+           f'    {self.__boost_create_info} |> with_view() <| $(vk_info)',
+           f'        var result_ = VkResult VK_SUCCESS',
+           f'        result ?? result_ = self.__vk_ctor_name(',
+        ]
+        params = []
+        for param in self.__vk_ctor_params:
+            if param.vk.name == self.__p_create_info:
+                params.append('safe_addr(vk_info)')
+            elif param.vk.name == self.__p_handle:
+                params.append(
+                    f'safe_addr({self.__boost_attr}.{self.__boost_attr})')
+            elif param.vk.name == 'pAllocator':
+                params.append('null')
+            else:
+                raise Exception(f'TODO: add support for extra param '
+                    f'{param.vk.name}')
+                params.append(param.boost.vk_value)
+        params_text = ', '.join(params)
+        lines + [
+           f'            {params_text}',
+           f'        )',
+           f'        assert(result_ == VkResult VK_SUCCESS)',
+           f'    return <- {self.__boost_attr}',
+        ]
+        return lines
+
+    def __generate_dtor(self):
+        lines = []
+        lines += [
+           f'def finalize(var {self.__boost_attr} : {self.__boost_type})',
+           f'    self.__vk_dtor_name(',
+        ]
+        params = []
+        for param in self.__vk_dtor_params:
+            if param.vk.name == self.__p_handle:
+                params.append(f'{self.__boost_attr}.{self.__boost_attr}')
+            elif param.vk.name == 'pAllocator':
+                params.append('null')
+            else:
+                raise Exception(f'TODO: add support for extra param '
+                    f'{param.vk.name}')
+        params_text = ', '.join(params)
+        lines += [
+           f'        {params_text}',
+           f'    )',
+           f'    memzero({self.__boost_attr})',
         ]
         return lines
 
