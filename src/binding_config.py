db27168b21c7996c5175de2aa7ff859ddb2a6a16
Oleg Plakhotniuk <olegus8@gmail.com>
2020-12-01 20:58:18 -0500

    working on modules

--- src/binding_config.py
+++ src/binding_config.py
@@ -117,6 +117,15 @@ def add_boost_content(g):
         struct      = 'VkExtensionProperties',
         vk_to_boost = True)
     g.add_gen_struct(
+        struct      = 'VkPhysicalDeviceProperties',
+        vk_to_boost = True)
+    g.add_gen_struct(
+        struct      = 'VkPhysicalDeviceSparseProperties',
+        vk_to_boost = True)
+    g.add_gen_struct(
+        struct      = 'VkPhysicalDeviceLimits',
+        vk_to_boost = True)
+    g.add_gen_struct(
         struct      = 'VkInstanceCreateInfo',
         boost_to_vk = True
         ).declare_array(
