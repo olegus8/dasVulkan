3a82842a3559ff86cf1939dbc9f5752ac611dac0
Oleg Plakhotniuk <olegus8@gmail.com>
2020-12-07 11:05:16 -0500

    working on modules

--- src/binding_config.py
+++ src/binding_config.py
@@ -112,6 +112,10 @@ def add_boost_content(g):
         ctor            = 'vkCreateFence',
         dtor            = 'vkDestroyFence')
     g.add_gen_handle(
+        handle          = 'VkImage',
+        ctor            = 'vkCreateImage',
+        dtor            = 'vkDestroyImage')
+    g.add_gen_handle(
         handle          = 'VkInstance',
         ctor            = 'vkCreateInstance',
         dtor            = 'vkDestroyInstance')
