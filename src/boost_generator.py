b2a0bd8cdcd91af21b3f70c86aeac7db6a3566d8
Oleg Plakhotniuk <olegus8@gmail.com>
2020-12-09 17:25:59 -0500

    working on modules

--- src/boost_generator.py
+++ src/boost_generator.py
@@ -147,20 +147,18 @@ class GenFunc(object):
 
     @property
     def _return_type(self):
-        outputs = self.__output_params
-        if len(outputs) > 1:
+        if len(self.__output_params) > 1:
             raise Exception('TODO: add multiple outputs support if needed')
-        return output[0]._boost_func_param_type if outputs else 'void'
+        for output in self.__output_params:
+            return output._boost_func_param_type
+        return 'void'
 
     @property
     def __return_value(self):
-        outputs = self.__output_params
-        if len(outputs) > 1:
         if len(self.__output_params) > 1:
             raise Exception('TODO: add multiple outputs support if needed')
-        rvalues = [t for param in self._output_params
-            for t in param.generate_boost_func_return_values]
-        return (rvalues or [None])[0]
+        for output in self.__output_params:
+            return output.boost_func_return_value
 
     def generate(self):
         lines = []
@@ -895,13 +893,8 @@ class ParamBase(object):
             return []
         return [f'{self._boost_func_param_name},']
 
-    def generate_boost_func_return_types(self):
-        if self._is_boost_func_output:
-            return [self._boost_func_param_type]
-        return []
-
     @property
-    def _boost_func_return_value(self):
+    def boost_func_return_value(self):
         bname = self._boost_func_param_name
         if self._vk_is_dyn_array_items:
             return [f'<- [{{for x in vk_{bname}; vk_value_to_boost(x)}}]']
