8c7b16824b7604c559ed8190ca186de9f59875d0
Oleg Plakhotniuk <olegus8@gmail.com>
2020-12-12 16:58:22 -0500

    working on modules

--- src/binding_config.py
+++ src/binding_config.py
@@ -160,6 +160,7 @@ def add_boost_content(g):
         'VkCommandPoolCreateInfo',
         'VkFenceCreateInfo',
         'VkImageViewCreateInfo',
+        'VkPipelineDepthStencilStateCreateInfo',
         'VkPipelineInputAssemblyStateCreateInfo',
         'VkPipelineMultisampleStateCreateInfo',
         'VkPipelineRasterizationStateCreateInfo',
