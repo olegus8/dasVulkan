8ca6fbaf24ad03d04eefd56cd43f170922238c3f
Oleg Plakhotniuk <olegus8@gmail.com>
2020-12-12 07:39:25 -0500

    working on modules

--- src/boost_generator.py
+++ src/boost_generator.py
@@ -989,6 +989,10 @@ class ParamVk_pNext(ParamBase):
         if c_param.name == 'pNext':
             return cls(c_param=c_param, **kwargs)
 
+    @property
+    def vk_unqual_type(self):
+        return self._c_unqual_type
+
     def generate_boost_struct_field_decl(self):
         return []
 
@@ -1009,13 +1013,14 @@ class ParamVk_sType(ParamBase):
         if c_param.name == 'sType':
             return cls(c_param=c_param, **kwargs)
 
+    @property
+    def vk_unqual_type(self):
+        return self._c_unqual_type
+
     def generate_boost_struct_field_decl(self):
         return []
 
     def generate_boost_struct_v2b_field(self):
-        return []
-
-    def generate_boost_struct_v2b_vars(self):
         return []
 
     def generate_boost_struct_view_create_init(self):
