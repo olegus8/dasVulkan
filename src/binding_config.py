384105221497fd1a6c341ec05c5d94427c384ab3
Oleg Plakhotniuk <olegus8@gmail.com>
2020-12-13 19:02:59 -0500

    working on modules

--- src/binding_config.py
+++ src/binding_config.py
@@ -274,3 +274,4 @@ def add_boost_content(g):
     g.add_gen_func(name = 'vkGetSwapchainImagesKHR',
         ).declare_array(count = 'pSwapchainImageCount', items = 'pSwapchainImages',
         ).declare_output(name = 'pSwapchainImages')
+    g.add_gen_func(name = 'vkQueueWaitIdle')
