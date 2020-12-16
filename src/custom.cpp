8d457dd8f80ab02a873c426612f2d7f27302c475
Oleg Plakhotniuk <olegus8@gmail.com>
2020-12-16 06:24:03 -0500

    working on modules

--- src/custom.cpp
+++ src/custom.cpp
@@ -1,6 +1,20 @@
 #include "daScript/daScript.h"
 
+#define GLFW_INCLUDE_VULKAN
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+
 using namespace das;
+
+GLFWwindow* glfw_create_window(int width, int height, const char* title,
+    GLFWmonitor* monitor, GLFWwindow* share
+) {
+    return glfwCreateWindow(width, height, title, monitor, share);
+}
+
+void glfw_destroy_window(GLFWwindow* window) {
+    glfwDestroyWindow(window);
+}
 
 void my_cpp_func(Context * ctx) {
     auto fx = ctx->findFunction("my_das_func");
@@ -14,4 +28,10 @@ void my_cpp_func(Context * ctx) {
 void addVulkanCustom(Module & module, ModuleLibrary & lib) {
     addExtern<DAS_BIND_FUN(my_cpp_func)>(module, lib, "my_cpp_func",
         SideEffects::worstDefault, "my_cpp_func");
+    addExtern<DAS_BIND_FUN(glfw_create_window)>(
+        *this, lib, "glfwCreateWindow",
+        SideEffects::worstDefault, "glfwCreateWindow");
+    addExtern<DAS_BIND_FUN(glfw_destroy_window)>(
+        *this, lib, "glfwDestroyWindow",
+        SideEffects::worstDefault, "glfwDestroyWindow");
 }
