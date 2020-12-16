97676ff2b258cd84d69deea3036dcd8c12df74ba
Oleg Plakhotniuk <olegus8@gmail.com>
2020-12-16 06:48:46 -0500

    working on modules

--- src/custom.cpp
+++ src/custom.cpp
@@ -2,16 +2,5 @@
 
 using namespace das;
 
-void my_cpp_func(Context * ctx) {
-    auto fx = ctx->findFunction("my_das_func");
-    if (!fx) {
-        ctx->throw_error("function not found");
-        return;
-    }
-    ctx->eval(fx, nullptr);
-}
-
 void addVulkanCustom(Module & module, ModuleLibrary & lib) {
-    addExtern<DAS_BIND_FUN(my_cpp_func)>(module, lib, "my_cpp_func",
-        SideEffects::worstDefault, "my_cpp_func");
 }
