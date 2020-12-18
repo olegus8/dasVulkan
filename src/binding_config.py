11a669273eb6d7c6ea32789426aa988611285cd1
Oleg Plakhotniuk <olegus8@gmail.com>
2020-12-18 08:28:15 -0500

    working on modules

--- src/binding_config.py
+++ src/binding_config.py
@@ -179,6 +179,7 @@ def add_boost_content(g):
 
     for name in [
         'VkBufferCopy',
+        'VkBufferMemoryBarrier',
         'VkBufferViewCreateInfo',
         'VkClearDepthStencilValue',
         'VkCommandBufferAllocateInfo',
