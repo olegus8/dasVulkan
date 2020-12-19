b57e3049549063a7767f5e41f4f5e1331ed2d827
Oleg Plakhotniuk <olegus8@gmail.com>
2020-12-19 07:28:55 -0500

    working on modules

--- example/vert.glsl
+++ example/vert.glsl
@@ -10,8 +10,10 @@ layout(location = 1) in vec3 in_color;
 layout(location = 2) in vec2 in_tex_pos;
 
 layout(location = 0) out vec3 frag_color;
+layout(location = 1) out vec2 frag_tex_pos;
 
 void main() {
     gl_Position = vec4(in_pos + ubo.offset, 0.0, 1.0);
     frag_color = in_color;
+    frag_tex_pos = tex_pos;
 }
