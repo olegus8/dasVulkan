6068325ce154ffccb16af27968789a87562e8d8d
Oleg Plakhotniuk <olegus8@gmail.com>
2020-12-19 07:28:06 -0500

    working on modules

--- example/vert.glsl
+++ example/vert.glsl
@@ -5,12 +5,13 @@ layout(binding = 0) uniform UniformBufferObject {
     vec2 offset;
 } ubo;
 
-layout(location = 0) in vec2 in_position;
+layout(location = 0) in vec2 in_pos;
 layout(location = 1) in vec3 in_color;
+layout(location = 2) in vec2 in_tex_pos;
 
 layout(location = 0) out vec3 frag_color;
 
 void main() {
-    gl_Position = vec4(in_position + ubo.offset, 0.0, 1.0);
+    gl_Position = vec4(in_pos + ubo.offset, 0.0, 1.0);
     frag_color = in_color;
 }
