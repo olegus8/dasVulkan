e872a57f45eb33f1f2c45af0e711448e7f73d0d9
Oleg Plakhotniuk <olegus8@gmail.com>
2020-12-19 07:47:15 -0500

    working on modules

--- example/frag.glsl
+++ example/frag.glsl
@@ -9,6 +9,5 @@ layout(location = 1) in vec2 frag_tex_pos;
 layout(location = 0) out vec4 out_color;
 
 void main() {
-    //out_color = vec4(frag_color, 1.0) * texture(tex_sampler, frag_tex_pos);
-    out_color = texture(tex_sampler, frag_tex_pos);
+    out_color = vec4(frag_color, 1.0) * texture(tex_sampler, frag_tex_pos);
 }
