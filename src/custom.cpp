a425a9699fde0284644621f6abce26ef263531d7
Oleg Plakhotniuk <olegus8@gmail.com>
2021-01-22 12:13:23 -0500

    working on it

--- src/custom.cpp
+++ src/custom.cpp
@@ -3,6 +3,10 @@
 typedef DebugMsgContext * DebugMsgContext_DasHandle;
 MAKE_TYPE_FACTORY(DebugMsgContext_DasHandle, DebugMsgContext_DasHandle)
 
+typedef DebugMsgContext * (pfn_create_debug_msg_context)(Lambda callback, Context * ctx);
+
+pfn_create_debug_msg_context g_p_create_debug_msg_context = nullptr;
+
 DebugMsgContext * create_debug_msg_context(Lambda callback, Context * ctx) {
     auto debug_ctx = new DebugMsgContext();
     debug_ctx->ctx = ctx;
@@ -35,12 +39,14 @@ void destroy_debug_msg_context(DebugMsgContext * debug_ctx, Context * ctx) {
 
 void addVulkanCustomBeforeGenerated(Module & module, ModuleLibrary & lib) {
 
+    g_p_create_debug_msg_context = &create_debug_msg_context;
+
     module.addAnnotation(make_smart<VkHandleAnnotation<
         DebugMsgContext_DasHandle> >(
             "DebugMsgContext_DasHandle",
             "DebugMsgContext_DasHandle"
     ));
-    addExtern<DAS_BIND_FUN(create_debug_msg_context)>(
+    addExtern<DAS_BIND_FUN((*g_p_create_debug_msg_context))>(
         module, lib, "create_debug_msg_context",
         SideEffects::worstDefault, "create_debug_msg_context");
     addExtern<DAS_BIND_FUN(destroy_debug_msg_context)>(
