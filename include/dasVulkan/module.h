60bf56c54eb4c57754d03c6338188e7df491938b
Oleg Plakhotniuk <olegus8@gmail.com>
2021-02-06 17:50:13 -0500

    working on it

--- include/dasVulkan/module.h
+++ include/dasVulkan/module.h
@@ -1,12 +1,4 @@
 #pragma once
 
 #include "module_custom.h"
-#include "module_generated.h.inc_enums_1"
-#include "module_generated.h.inc_enums_2"
-#include "module_generated.h.inc_enums_3"
-#include "module_generated.h.inc_enums_4"
-#include "module_generated.h.inc_enums_5"
-#include "module_generated.h.inc_enums_6"
-#include "module_generated.h.inc_enums_7"
-#include "module_generated.h.inc_enums_8"
-#include "module_generated.h.inc"
+#include "module_generated.h"
