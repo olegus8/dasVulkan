ca23ee2d10cae40c150adefbb542d4ea7cfea19c
Oleg Plakhotniuk <olegus8@gmail.com>
2025-05-15 14:09:55 -0400

    working on it

--- src/binding_config.py
+++ src/binding_config.py
@@ -174,6 +174,7 @@ def add_boost_content(g):
         'VkPhysicalDeviceLimits',
         'VkPhysicalDeviceProperties',
         'VkPhysicalDeviceSparseProperties',
+        'VkPipelineExecutablePropertiesKHR',
         'VkPipelineInfo',
         'VkPushConstantRange',
         'VkQueueFamilyProperties',
