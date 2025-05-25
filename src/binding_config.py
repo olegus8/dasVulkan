02f7b192fb1f6328d9ab223ec2ae41953159b1bd
Oleg Plakhotniuk <olegus8@gmail.com>
2025-05-25 14:09:57 -0400

    working on it

--- src/binding_config.py
+++ src/binding_config.py
@@ -263,7 +263,7 @@ def add_boost_content(g):
         ).next_in_chain(phys_dev_12_feats)
     wds_accel = g.add_gen_struct(name = 'VkWriteDescriptorSetAccelerationStructureKHR', vk_to_boost=False,
         ).declare_array(count = 'accelerationStructureCount', items = 'pAccelerationStructures')
-    wds_inline_uni = g.add_gen_struct(name = 'VkWriteDescriptorSetInlineUniformBlockEXT', vk_to_boost=False)
+    wds_inline_uni = g.add_gen_struct(name = 'VkWriteDescriptorSetInlineUniformBlock', vk_to_boost=False)
     inline_uniform_dp = g.add_gen_struct(name = 'VkDescriptorPoolInlineUniformBlockCreateInfoEXT', vk_to_boost=False)
 
     g.add_gen_struct(name = 'VkAccelerationStructureBuildGeometryInfoKHR', vk_to_boost=False, ignore_fields=['ppGeometries']
