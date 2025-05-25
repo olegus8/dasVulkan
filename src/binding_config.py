de0d738d0f73232008921df6232f9a563bfe814b
Oleg Plakhotniuk <olegus8@gmail.com>
2025-05-25 13:27:24 -0400

    working on it

--- src/binding_config.py
+++ src/binding_config.py
@@ -362,10 +362,6 @@ def add_boost_content(g):
         ).declare_array(count = 'descriptorCount', items = 'pBufferInfo', optional=True,
         ).declare_array(count = 'descriptorCount', items = 'pTexelBufferView', optional=True)
 
-    g.add_gen_struct(name = 'VkPhysicalDeviceFeatures2',
-        ).next_in_chain(g.add_gen_struct(
-            name = 'VkPhysicalDeviceVulkan11Features'))
-
     #
     # Functions
     #
